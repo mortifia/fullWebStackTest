330816ed90596572abe6e8241509fd5048aa1300
mortifia <amela57290@gmail.com>
2022-01-30 09:19:39 +0100

    ca communique ^^

--- front/heuuu/pages/index.tsx
+++ front/heuuu/pages/index.tsx
@@ -7,16 +7,37 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import React from 'react'
+import internal from 'stream'
+import { article } from '../../../back/api/src/schema/article'
+
+type article = {
+  id: number
+  on: string
+  text: string
+}
 
 export const getServerSideProps = async () => {
+  const articles = (
+    await (
+      await fetch('http://localhost:3010/', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query: `{ articles{ id on text}}` }),
+      })
+    ).json()
+  ).data.articles as article[]
   return {
     props: {
-      Heuuu: { p1: 'oui on vous entend ' },
+      articles,
     },
   }
 }
 
 function Home(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const dateTimezone = (date: string) => {
+    const old = new Date(date)
+    return new Date(old.getTime() - old.getTimezoneOffset() * 60 * 1000)
+  }
   return (
     <div className={styles.container}>
       <Head>
@@ -28,7 +49,16 @@ function Home(props: InferGetServerSidePropsType<typeof getServerSideProps>) {
       <main className={styles.main}>
         <div>coucou</div>
         <div>heuuu 1. 2. ? </div>
-        <div>{props.Heuuu.p1}</div>
+        <div>
+          {props.articles.map(article => {
+            return (
+              <div key={article.id}>
+                id: {article.id} | on: {article.on}| test:
+                {dateTimezone(article.on).toUTCString()} | text: {article.text}
+              </div>
+            )
+          })}
+        </div>
       </main>
       <div></div>
 
--- front/heuuu/src/theme.ts
+++ front/heuuu/src/theme.ts
@@ -1,40 +1,41 @@
-import { createTheme, ThemeOptions } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+export {}
+// import { createTheme, ThemeOptions } from '@mui/material/styles'
+// import { red } from '@mui/material/colors'
 
-export const themeOptionsDark: ThemeOptions = {
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#e07606',
-    },
-    secondary: {
-      main: '#f50057',
-    },
-    background: {
-      paper: '#303030',
-      default: '#252525',
-    },
-  },
-}
+// export const themeOptionsDark: ThemeOptions = {
+//   palette: {
+//     mode: 'dark',
+//     primary: {
+//       main: '#e07606',
+//     },
+//     secondary: {
+//       main: '#f50057',
+//     },
+//     background: {
+//       paper: '#303030',
+//       default: '#252525',
+//     },
+//   },
+// }
 
-export const themeOptions: ThemeOptions = {
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#fa9b47',
-    },
-    secondary: {
-      main: '#f44336',
-    },
-  },
-  typography: {
-    fontFamily: '"Mulish", "Roboto", "Helvetica", "Arial", sans-serif',
-    fontSize: 16,
-  },
-}
+// export const themeOptions: ThemeOptions = {
+//   palette: {
+//     mode: 'dark',
+//     primary: {
+//       main: '#fa9b47',
+//     },
+//     secondary: {
+//       main: '#f44336',
+//     },
+//   },
+//   typography: {
+//     fontFamily: '"Mulish", "Roboto", "Helvetica", "Arial", sans-serif',
+//     fontSize: 16,
+//   },
+// }
 
-// Create a theme instance.
+// // Create a theme instance.
 
-export const theme = createTheme(themeOptions)
+// export const theme = createTheme(themeOptions)
 
-export default theme
+// export default theme
